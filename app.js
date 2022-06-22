43e1f5a1d99ed803a3611ae680e3539938803a95
Shawky Faried elshalawy <85047354+ShawkyElshawlay@users.noreply.github.com>
2022-06-22 18:42:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/errorhandler');
+
+app.use(cors());
+app.options('*', cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
+
+//Routes
+const categoriesRoutes = require('./routers/categories');
+const productsRoutes = require('./routers/products');
+const usersRoutes = require('./routers/users');
+const ordersRoutes = require('./routers/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: process.env.DB_NAME
+    })
+    .then(() => {
+        console.log('we are using ' + process.env.DB_NAME);
+        console.log('Database Connection is ready...');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+const PORT = process.env.PORT || 3000;
+//Server
+app.listen(PORT, () => {
+    console.log('server is running http://localhost:3000');
+});
