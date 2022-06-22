d63275c901f24ed35d67c12dced75ab9ab80d1b0
Shawky Faried elshalawy <85047354+ShawkyElshawlay@users.noreply.github.com>
2022-06-22 18:53:51 +0200

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,7 +3,7 @@ const expressJwt = require('express-jwt');
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
-    return expressJwt.expressjwt({
+    return expressJwt({
         secret,
         algorithms: ['HS256'],
         isRevoked: isRevoked
@@ -12,23 +12,20 @@ function authJwt() {
             { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/orders(.*)/, methods: ['POST', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             `${api}/users/login`,
             `${api}/users/register`
-            
+            // { url: /(.*)/ },
         ]
     });
 }
 
 async function isRevoked(req, payload, done) {
-    console.log(req.url);
-    console.log(req.method);
-    if (payload.isAdmin) {
-        done();
-    } else if (req.url.include('orders')) {
+    if (!payload.isAdmin) {
+        done(null, true);
     }
 
-    done(null, true);
+    done();
 }
 
 module.exports = authJwt;
